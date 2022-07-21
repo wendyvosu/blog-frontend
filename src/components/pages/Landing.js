88cdcb17a0b80e8ca882d60e36de354ad3ef8704
Wendy Vuong <vuongw@oregonstate.edu>
2022-07-21 08:58:57 -0400

    Add files via upload

---
+++ src/components/pages/Landing.js
@@ -0,0 +1,27 @@
+import RegisterForm from "../forms/RegisterForm";
+import LoginForm from "../forms/LoginForm";
+import { useState } from "react";
+
+const Landing = (props) => {
+  const [hasAccount, setHasAccount] = useState(false);
+  const {setUser} = props
+  return (
+    <div>
+      <h1>Landing Page</h1>
+
+      {hasAccount === false ? (
+        <div>
+          <RegisterForm  setUser={setUser} />
+          <p>
+            Already have an account?{" "}
+            <span className="btn btn-primary" onClick={() => setHasAccount(true)}>Login</span>{" "}
+          </p>
+        </div>
+      ) : (
+        <LoginForm setUser={setUser} />
+      )}
+    </div>
+  );
+};
+
+export default Landing;
